704882dd9a3e974bc023d159fa2b6ec1d38d74a8
Francis Carino <fgcarino@gmail.com>
2022-07-28 12:00:27 -0700

    add start surevey and go to question functions

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from http.client import responses
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
@@ -7,3 +8,16 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.get('/')
+def start_survey():
+
+    return render_template("/survey_start.html", survey = survey)
+
+
+@app.post("/begin")
+def go_to_questions():
+
+    return redirect("/questions/0")
